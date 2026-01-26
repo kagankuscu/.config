8ff5b5a90b9749647a0e528a3e30e758bf811728
kagankuscu <kuscukagan@gmail.com>
2026-01-26 16:15:10 +0200

    Modify font settings and add tab navigation keys
    Updated font settings and added key bindings for tab navigation.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,9 +1,22 @@
 local wezterm = require 'wezterm'
 
+local act = wezterm.action
 local config = wezterm.config_builder()
 
+-- theme and tab settings
 config.hide_tab_bar_if_only_one_tab = true
 config.color_scheme = 'Catppuccin Mocha'
-config.font = wezterm.font 'Fira Code'
+
+-- font settings
+config.font = wezterm.font("Fira Code", {weight="Regular", stretch="Normal", style="Normal"}) -- /usr/share/fonts/truetype/firacode/FiraCode-Regular.ttf, FontConfig
+config.font_size = 12.0
+
+
+local mykeys = {}
+-- switch active tab
+table.insert(mykeys, { key = '[', mods = 'CTRL|ALT', action = act.ActivateTabRelative(-1) })
+table.insert(mykeys, { key = ']', mods = 'CTRL|ALT', action = act.ActivateTabRelative(1) })
+
+config.keys = mykeys
 
 return config
